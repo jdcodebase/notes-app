439dd72d709403cfd5b15908b215d58cd070d68a
jdcodebase <jd.codebase@gmail.com>
2026-07-26 12:02:02 +0530

    cors error fix

--- server/src/app.js
+++ server/src/app.js
@@ -7,10 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://notes-web-app-nine.vercel.app/",
+    origin: process.env.CLIENT_URL,
+    credentials: true,
   }),
 );
-
 app.get("/", (req, res) => {
   res.send("API Running successfully");
 });
